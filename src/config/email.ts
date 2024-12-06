67923cd436f53330d66acb84fee275919601643e
VermaAdarsh2k01 <verma.adarsh2510@gmail.com>
2024-12-06 21:02:57 +0530

    Form data and ui

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,11 +1,10 @@
-interface EmailConfig {
-  serviceId: string;
-  templateId: string;
-  publicKey: string;
-}
+import emailjs from '@emailjs/browser';
 
-export const emailConfig: EmailConfig = {
+emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
+
+
+export const emailConfig = {
   serviceId: import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
   templateId: import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
-  publicKey: import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY
+  publicKey: import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY,
 }; 
